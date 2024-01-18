862825f48ad19bccb652099caedda56ee5d263d1
cactusbento <cactusbento@outlook.com>
2024-01-17 20:48:23 -0500

    Install/Uninstall implemented

--- src/main.zig
+++ src/main.zig
@@ -55,52 +55,59 @@ pub fn main() !void {
             try bw.flush();
             return error.InvalidCommand;
         };
+        var packages = std.MultiArrayList(Package){};
+        defer {
+            var slice = packages.slice();
+            for (slice.items(.name), slice.items(.category), slice.items(.description)) |name, cat, desc| {
+                allocator.free(name);
+                allocator.free(cat);
+                allocator.free(desc);
+            }
+            slice.deinit(allocator);
+        }
+
+        var pkgsrc_iter = pkgsrc.iterate();
+        while (try pkgsrc_iter.nextLinux()) |cat_entry| {
+            if (cat_entry.kind != .directory) continue; // ignore files
+            var cat = try pkgsrc.openDir(cat_entry.name, .{ .iterate = true });
+            defer cat.close();
+
+            var cat_iter = cat.iterate();
+            while (try cat_iter.nextLinux()) |pkg_entry| {
+                if (pkg_entry.kind != .directory) continue; // ignore files
+                var pkg = try cat.openDir(pkg_entry.name, .{});
+                defer pkg.close();
+
+                _ = pkg.statFile("DESCR") catch |err| switch (err) {
+                    error.FileNotFound => continue,
+                    else => |e| return e,
+                };
+
+                try packages.append(allocator, try Package.read(
+                    allocator,
+                    cat_entry.name,
+                    pkg_entry.name,
+                    pkg,
+                ));
+            }
+        }
         switch (cmd) {
             .search => {
-                var packages = std.MultiArrayList(Package){};
-                defer {
-                    var slice = packages.slice();
-                    for (slice.items(.name), slice.items(.category), slice.items(.description)) |name, cat, desc| {
-                        allocator.free(name);
-                        allocator.free(cat);
-                        allocator.free(desc);
-                    }
-                    slice.deinit(allocator);
-                }
-
-                var pkgsrc_iter = pkgsrc.iterate();
-                while (try pkgsrc_iter.nextLinux()) |cat_entry| {
-                    if (cat_entry.kind != .directory) continue; // ignore files
-                    var cat = try pkgsrc.openDir(cat_entry.name, .{ .iterate = true });
-                    defer cat.close();
-
-                    var cat_iter = cat.iterate();
-                    while (try cat_iter.nextLinux()) |pkg_entry| {
-                        if (pkg_entry.kind != .directory) continue; // ignore files
-                        var pkg = try cat.openDir(pkg_entry.name, .{});
-                        defer pkg.close();
-
-                        _ = pkg.statFile("DESCR") catch |err| switch (err) {
-                            error.FileNotFound => continue,
-                            else => |e| return e,
-                        };
-
-                        try packages.append(allocator, try Package.read(
-                            allocator,
-                            cat_entry.name,
-                            pkg_entry.name,
-                            pkg,
-                        ));
-                    }
-                }
                 var index_List = std.ArrayList(usize).init(allocator);
                 defer index_List.deinit();
 
                 for (packages.items(.name), packages.items(.description), 0..) |name, desc, i| {
                     const search_terms = argv[2..];
                     for (search_terms) |term| {
-                        const in_name = std.mem.indexOf(u8, name, term) != null;
-                        const in_desc = std.mem.indexOf(u8, desc, term) != null;
+                        const lc_name = try std.ascii.allocLowerString(allocator, name);
+                        defer allocator.free(lc_name);
+                        const lc_desc = try std.ascii.allocLowerString(allocator, desc);
+                        defer allocator.free(lc_desc);
+                        const lc_term = try std.ascii.allocLowerString(allocator, term);
+                        defer allocator.free(lc_term);
+
+                        const in_name = std.mem.indexOf(u8, lc_name, lc_term) != null;
+                        const in_desc = std.mem.indexOf(u8, lc_desc, lc_term) != null;
 
                         if (in_name or in_desc) {
                             try index_List.append(i);
@@ -126,8 +133,121 @@ pub fn main() !void {
                 }
                 try bw.flush();
             },
-            .install => {},
-            .uninstall => {},
+            .install => {
+                const which_res = try std.process.Child.run(.{
+                    .allocator = allocator,
+                    .argv = &.{ "which", "bmake" },
+                });
+                defer {
+                    allocator.free(which_res.stdout);
+                    allocator.free(which_res.stderr);
+                }
+                if (which_res.term != .Exited) {
+                    try stdout.print(
+                        \\ Command "bmake" not found.
+                        \\
+                        \\ Is "pkgsrc" installed?
+                        \\ Is "bmake" in your PATH?
+                    , .{});
+                    try bw.flush();
+                    return error.CommandNotFound;
+                }
+                const pkgs_to_install = argv[2..];
+                for (pkgs_to_install) |pkg_name| {
+                    const pkg_index = indexOfSlice(u8, packages.items(.name), pkg_name) orelse {
+                        try stdout.print("Package not found: {s}\n", .{pkg_name});
+                        try bw.flush();
+                        return error.PackageNotFound;
+                    };
+
+                    const pkg_path = try std.fmt.allocPrint(allocator, "{s}/{s}", .{
+                        packages.items(.category)[pkg_index],
+                        packages.items(.name)[pkg_index],
+                    });
+                    defer allocator.free(pkg_path);
+
+                    var pkg_dir = try pkgsrc.openDir(pkg_path, .{ .iterate = true });
+                    defer pkg_dir.close();
+
+                    const bmake_res = try std.process.Child.run(.{
+                        .allocator = allocator,
+                        .argv = &.{"bmake"},
+                        .cwd_dir = pkg_dir,
+                    });
+                    defer {
+                        allocator.free(bmake_res.stdout);
+                        allocator.free(bmake_res.stderr);
+                    }
+                    if (bmake_res.term != .Exited) {
+                        try stdout.print("bmake failed: \n{s}\n", .{bmake_res.stdout});
+                        try bw.flush();
+                        return error.bmakeFail;
+                    }
+
+                    const bmake_install_res = try std.process.Child.run(.{
+                        .allocator = allocator,
+                        .argv = &.{ "bmake", "install" },
+                        .cwd_dir = pkg_dir,
+                    });
+                    defer {
+                        allocator.free(bmake_install_res.stdout);
+                        allocator.free(bmake_install_res.stderr);
+                    }
+                    if (bmake_install_res.term != .Exited) {
+                        try stdout.print("bmake install failed: \n{s}\n", .{bmake_install_res.stdout});
+                        try bw.flush();
+                        return error.bmakeInstallFail;
+                    }
+
+                    const bmake_clean_res = try std.process.Child.run(.{
+                        .allocator = allocator,
+                        .argv = &.{ "bmake", "clean" },
+                        .cwd_dir = pkg_dir,
+                    });
+                    defer {
+                        allocator.free(bmake_clean_res.stdout);
+                        allocator.free(bmake_clean_res.stderr);
+                    }
+                    if (bmake_clean_res.term != .Exited) {
+                        try stdout.print("bmake clean failed: \n{s}\n", .{bmake_clean_res.stdout});
+                        try bw.flush();
+                        return error.bmakeCleanFail;
+                    }
+
+                    const bmake_cleandep_res = try std.process.Child.run(.{
+                        .allocator = allocator,
+                        .argv = &.{ "bmake", "clean-depends" },
+                        .cwd_dir = pkg_dir,
+                    });
+                    defer {
+                        allocator.free(bmake_cleandep_res.stdout);
+                        allocator.free(bmake_cleandep_res.stderr);
+                    }
+                    if (bmake_cleandep_res.term != .Exited) {
+                        try stdout.print("bmake clean-depends failed: \n{s}\n", .{bmake_cleandep_res.stdout});
+                        try bw.flush();
+                        return error.bmakeCleanDependsFail;
+                    }
+                }
+            },
+            .uninstall => {
+                const pkgs_to_uninstall = argv[2..];
+                for (pkgs_to_uninstall) |pkg_name| {
+                    const pkg_delete_res = try std.process.Child.run(.{
+                        .allocator = allocator,
+                        .argv = &.{ "pkg_delete", pkg_name },
+                    });
+                    defer {
+                        allocator.free(pkg_delete_res.stdout);
+                        allocator.free(pkg_delete_res.stderr);
+                    }
+                    if (pkg_delete_res.term != .Exited) {
+                        try stdout.print("pkg_delete failed: \n{s}\n", .{pkg_delete_res.stdout});
+                        try bw.flush();
+                        return error.pkgDeleteFail;
+                    }
+                }
+            },
         }
     } else {
         try stdout.print("{s}\n", .{pkgsrcloc});
@@ -135,6 +255,15 @@ pub fn main() !void {
 
         return error.PKGSRCLOCNotSet;
     }
+}
+
+fn indexOfSlice(comptime T: type, a: []const []const T, b: []const T) ?usize {
+    for (a, 0..) |str, i| {
+        if (std.mem.eql(T, str, b)) {
+            return i;
+        }
+    }
+    return null;
 }
 
 const Command = enum {
